a40b6da2899bec86d652112d332ce97aa50cd43c
Paul Wlodkowski <PaulWlodkowski@Pauls-MacBook-Pro-2.local>
2018-12-15 19:15:40 +0100

    modified config_template with more detailed instructions

--- config_template.py
+++ config_template.py
@@ -3,15 +3,19 @@ import pymongo
 """
    Variables that contain user credentials for Twitter API and Mongo Atlas DB.
 
-   Intructions:
+    Intructions:
 
-   - paste your own Twitter credentials and Mongo Atlas Cloud DB
+    - paste your own Twitter credentials and Mongo Atlas Cloud DB
      username / password into the empty strings.
 
-   - you can find the short_srv_suffix by going into your Mongo Atlas DB cluster
+    - you can find the SHORT_SRV_SUFFIX by going into your Mongo Atlas DB cluster
      and clicking 'Connect' > 'Connect your application' and copying the part
      of the 'Short SRV connection string' after the '@' symbol.
      (e.g. 'myproject-rakqi.mongodb.net/test?retryWrites=true')
+
+    - the ATLAS_CLIENT is used for storing twitter data in the remote database, and the LOCAL_CLIENT is used for storing a local version of a "lookup" table of location names that are queried and geo-coded from the Nominatum API tool (https://wiki.openstreetmap.org/wiki/Nominatim). This variable is used in the geocoder.py file.
+
+        - The purpose of this strategy is to keep a local "cache" of geo-coded locations so that this program limits the amount of times it needs to ping the Nominatim API through the geopy library (i.e. whenever the program is in the process of geo-coding the user location field of each collected tweet, it will first check if the user location is in the local database; if it's not, only then will it send a request to the API.)
 """
 
 
@@ -24,8 +28,9 @@ ACCESS_TOKEN_SECRET = ""
 
 ### MONGO ATLAS CLOUD DB ###
 
-db_username = ""
-db_password = ""
-short_srv_suffix = ""
+DB_USERNAME = ""
+DB_PASSWORD = ""
+SHORT_SRV_SUFFIX = ""
 
-client = pymongo.MongoClient(f"mongodb+srv://{db_username}:{db_password}@{short_srv_suffix}")
+ATLAS_CLIENT = pymongo.MongoClient(f"mongodb+srv://{DB_USERNAME}:{DB_PASSWORD}@{SHORT_SRV_SUFFIX}")
+LOCAL_CLIENT = pymongo.MongoClient()
